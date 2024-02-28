049691f39c70f9dcc4aa481e149d44f6f43d84e7
shiva pachika <shivapachika@gmail.com>
2024-02-28 11:52:05 -0800

    updated ansible artifacts

--- ansible/roles/jmeter/files/git-clone-jmeter-scripts.sh
+++
@@ -1,5 +0,0 @@
-cd /c/apps/jmeter/scripts/
-git clone https://github.berkeley.edu/SIS/sis-jmeter-scripts.git
-## will ask for Calnet ID/Password, will setup SSH token later
-
-### push will be done by QA team
